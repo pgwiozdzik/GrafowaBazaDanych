8f48586387454f127141fcf808285f4dd33a2ebb
piotr <piotr.gwiozdzik@gmail.com>
2025-12-02 09:59:08 +0100

    Fix Apollo imports for Vite build

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
-import { useQuery, gql } from '@apollo/client';
+import { gql } from '@apollo/client';
+import { useQuery } from '@apollo/client/react/hooks';
 
 // Zapytanie GraphQL - to tu dzieje się magia grafowa
 const GET_BOOKS = gql`
